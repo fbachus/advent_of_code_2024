d0a3d1519e612b57135186e7e4be364874be7380
fbachus <felix.bachstein@gmail.com>
2024-12-04 15:45:12 +0100

    add day 1 task two, w/ successful completion

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -16,13 +16,9 @@ fn main() {
     );
     first_col.sort();
     second_col.sort();
-    let result: usize = first_col
-        .iter()
-        .zip(second_col)
-        .map(|(x,y)| x.abs_diff(y))
-        .sum();
-    println!("{}", result);
-
+    let result_one = task_one(&first_col, &second_col);
+    let result_two = task_two(&first_col, &second_col);
+    println!("result one: {}\nresult two: {}", result_one, result_two);
 }
 
 fn parse_input(file: &str) -> Vec<String> {
@@ -32,4 +28,19 @@ fn parse_input(file: &str) -> Vec<String> {
         .map(ToString::to_string)
         .filter(|x| x != "")
         .collect::<Vec<String>>()
+}
+
+fn task_one(first_col: &Vec<usize>, second_col: &Vec<usize>) -> usize {
+    first_col
+        .iter()
+        .zip(second_col)
+        .map(|(x,&y)| x.abs_diff(y))
+        .sum()
+}
+
+fn task_two(first_col: &Vec<usize>, second_col: &Vec<usize>) -> usize {
+    first_col
+        .iter()
+        .map(|x| x * second_col.iter().filter(|&y| y == x).count())
+        .sum()
 }
