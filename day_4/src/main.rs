9d55812ccbc08d93ffc9ea55be654513a12b4f74
fbachus <felix.bachstein@gmail.com>
2024-12-14 19:06:17 +0100

    fix: day 4 task one working now

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,17 +1,18 @@
-// Current answer with input is 2511, which is too low
 use std::fs;
 use std::num::TryFromIntError;
 
+#[derive(Debug)]
 struct Position {
     x: usize,
     y: usize,
 }
 
-//#[derive(Clone)]
+#[derive(Debug)]
 struct PositionChange {
     x: isize,
     y: isize,
 }
+#[derive(Debug)]
 struct WordAssistant<'a> {
     position: Position,
     direction: PositionChange,
@@ -29,17 +30,17 @@ impl Position {
 
 impl WordAssistant <'_> {
     fn makes_valid_word(&mut self, field: &[Vec<char>]) -> bool {
-        if self.word_pos >= self.word.len() {
-            true
-        } else if self.position.y >= field.len() ||
+        if self.position.y >= field.len() ||
                   self.position.x >= field[self.position.y].len() {
             false
         } else if field[self.position.y][self.position.x] == self.word[self.word_pos] {
-            match self.position.move_position(&self.direction) {
-                Ok(_) => (),
-                Err(_) => return false,
-            }
             self.word_pos += 1;
+            if self.word_pos >= self.word.len() {
+                return true
+            }
+            else if self.position.move_position(&self.direction).is_err() { 
+                return false
+            }
             self.makes_valid_word(field)
         } else {
             false
@@ -50,7 +51,8 @@ impl WordAssistant <'_> {
 fn main() {
     let input = parse_input("input.txt");
     //let input = parse_input("day_4/input.txt");
-    let find_this = ['X', 'M', 'A', 'S'];
+    //let find_this = ['X', 'M', 'A', 'S'];
+    let find_this: Vec<char> = "XMAS".chars().collect();
     let res_one = task_one(&input, &find_this);
     let res_two = task_two(&input, &find_this);
     println!("Task One: {}\nTask Two: {}", res_one, res_two);
@@ -58,26 +60,25 @@ fn main() {
 
 fn task_one(input: &[Vec<char>], word_to_find: &[char]) -> usize {
     input.iter().enumerate().map(|(i, y)| {
-        y.iter().enumerate().filter_map(|(j, x)| {
-            if *x != 'X' { return None }
-            // generate directions to check in 
-            Some((-1..=1).map(move |move_x| {
-                (-1..=1).filter(move |&move_y| {
-                    if move_y == 0 && move_x == 0 { return false }
+        y.iter().enumerate()
+            .filter(|(_, x)| x == &&word_to_find[0])
+            .map(|(j,_)| { // generate directions to check in 
+                (-1..=1).map(move |move_y| {
+                    (-1..=1).map(move |move_x| (move_x, move_y))
+                })
+                .flatten()
+                .filter(move |(move_x, move_y)| {
+                    if move_y == &0 && move_x == &0 { return false }
                     let mut word = WordAssistant {
                         position: Position { x: j, y: i },
-                        direction: PositionChange { x: move_x, y: move_y },
+                        direction: PositionChange { x: *move_x, y: *move_y },
                         word: word_to_find,
                         word_pos: 0,
                     };
-                    word.makes_valid_word(&input)
-                }).count()
-            })
-                //.inspect(|c| println!("{}", c))
-                .sum::<usize>()
-            )
-        }).sum::<usize>()
-    }).sum()
+                    word.makes_valid_word(input)
+                }).count()  // count words found on one position
+            }).sum::<usize>() // sum up characters of one line
+    }).sum::<usize>() // sum up lines
 }
 
 fn task_two(input: &[Vec<char>], word_to_find: &[char]) -> usize {
